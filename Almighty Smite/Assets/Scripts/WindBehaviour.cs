59169a078989afdc6ea7f785ba02c3c352705c63
Felix Almqvist <kiwibirdi@hotmail.com>
2019-04-09 14:45:41 +0200

    folders v2

--- Almighty Smite/Assets/Scripts/WindBehaviour.cs
+++ Almighty Smite/Assets/Scripts/WindBehaviour.cs
@@ -8,12 +8,32 @@ public class WindBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        windDirection = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+        switch(windDirection)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+        }
     }
 }
