63f981c4dee2e3f36749f6c2e3d18043922b7f01
Felix Almqvist <kiwibirdi@hotmail.com>
2019-04-18 16:10:05 +0200

    testing some stash fixes

---
+++ Almighty Smite/Assets/Scripts/CityAlive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityAlive : MonoBehaviour
+{
+    private DamageDealer parentScript;
+    private float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentScript = GetComponentInParent<DamageDealer>();
+        Timer = 0.0f;
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag != "Land" && col.gameObject.tag != "MainCamera")
+        {
+            Timer += 1;
+            if (Timer > 5)
+            {
+                parentScript.OnChildTriggerEnter2D(col);
+                Timer = 0.0f;
+            }
+        }
+    }
+}
