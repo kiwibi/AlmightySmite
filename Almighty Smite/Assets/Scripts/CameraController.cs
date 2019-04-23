c34f76f4290f4d301806766f05e19f43490f5189
Emil Dymek <emil.dymek@gmail.com>
2019-04-23 15:43:37 +0200

    Camera wrap bug fixes
    Big boi brain moves

--- Almighty Smite/Assets/Scripts/CameraController.cs
+++ Almighty Smite/Assets/Scripts/CameraController.cs
@@ -10,10 +10,9 @@ public class CameraController : MonoBehaviour
     private readonly float MinX = -19.911f;                                      //This is the western border of the map
     private readonly float MaxY = 11.2f;                                         //This is the "roof" of the map
     private readonly float MinY = -11.2f;                                        //This is the "floor" of the map
+    private readonly float Wrap = 57.601f;
     private readonly float FarMaxX = 37.69f;                                     //This is one camera's space away from the eastern border
     private readonly float FarMinX = -37.69f;                                    //This is one camera's space away from the western border
-    private bool LeftCheck = true;                                               //Checks if the camera was just wrapped around on the left side.
-    private bool RightCheck = true;                                              //Checks if the camera was just wrapped around on the right side.
     [SerializeField] private float CameraSpeed = 10.0f;                          //Change this variable to change camera movement speed.
 
     Transform Camera1;                                                           //This variable stores the position of Camera 1
@@ -28,8 +27,8 @@ public class CameraController : MonoBehaviour
     void Update()                                                                //This function is called once per frame
     {
         GetInput();                                                              //Calls input function once per frame
-        CameraMovement();                                                        //Calls movement function once per frame
         CameraCheck();                                                           //Calls the camera loop function once per frame
+        CameraMovement();                                                        //Calls movement function once per frame
     }
 
     void GetInput()                                                              //This function checks for player input
@@ -64,27 +63,35 @@ public class CameraController : MonoBehaviour
         transform.Translate(CameraDirection * CameraSpeed * Time.deltaTime);     //Moves the camera according to direction and speed times time in seconds
     }
 
-    void CameraCheck()                                                                                                                          //This Function moves the individual cameras to wrap around the map.
+    void CameraCheck()                                                                                                                                   //This Function moves the individual cameras to wrap around the map.
     {
-        if (Camera1.position.x > MaxX && RightCheck == true)                                                                                    //Checks if Camera 1 has reached the right edge of the map
+        if (Camera1.position.x > MaxX)                                                                                                                   //If the main camera is over the right border
         {
-            Camera1.transform.SetPositionAndRotation(new Vector3(FarMinX, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));                //Moves Camera 1 to the opposite side of the map horizontally
-            LeftCheck = false;                                                                                                                  //Sets leftcheck to false to avoid conflicts with other if-statements
+            if (Camera1.position.x == Camera2.position.x)                                                                                                //If the main and secondary camera has the same position
+            {
+                Camera2.transform.SetPositionAndRotation(new Vector3(Camera1.position.x - Wrap, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));   //Move the secondary camera one "wrap distance" to the left
+            }
+            if (Camera1.position.x >= FarMaxX)                                                                                                           //If the main camera is off the right side of the map
+            {
+                Camera1.transform.SetPositionAndRotation(new Vector3(Camera2.position.x, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));          //Move the main camera to the secondary cameras position
+            }
         }
-        if (Camera2.position.x > FarMaxX)                                                                                                       //Checks if Camera 2 has wandered off the right side of the map
+
+        if (Camera1.position.x < MinX)                                                                                                                   //If the main camera is over the left border
         {
-            Camera2.transform.SetPositionAndRotation(new Vector3(Camera1.position.x, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));     //Moves Camera 2 to Camera 1
-            LeftCheck = true;                                                                                                                   //Resets LeftCheck
+            if (Camera1.position.x == Camera2.position.x)                                                                                                //If the main and secondary camera has the same position
+            {
+                Camera2.transform.SetPositionAndRotation(new Vector3(Camera1.position.x + Wrap, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));   //Move the secondary camera one "wrap distance" to the right
+            }
+            if (Camera1.position.x <= FarMinX)                                                                                                           //If the main camera is off the left side of the border
+            {
+                Camera1.transform.SetPositionAndRotation(new Vector3(Camera2.position.x, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));          //Move the main camera to the secondary cameras position
+            }
         }
-        if (Camera1.position.x < MinX && LeftCheck == true)                                                                                     //Checks if Camera 1 has reached the left edge of the map
+
+        if (Camera2.position.x < FarMinX || Camera2.position.x > FarMaxX)                                                                                //If the secondary camera is off the screen on any side
         {
-            Camera1.transform.SetPositionAndRotation(new Vector3(FarMaxX, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));                //Moves Camera 1 to the opposite side of the map horizontally
-            RightCheck = false;                                                                                                                 //Sets rightcheck to false to avoid conflicts
-        }
-        if (Camera2.position.x < FarMinX)                                                                                                       //Checks if Camera 2 has wandered of the left side of the map
-        {
-            Camera2.transform.SetPositionAndRotation(new Vector3(Camera1.position.x, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));     //Moves Camera 2 to Camera 1
-            RightCheck = true;                                                                                                                  //Resets RightCheck
+            Camera2.transform.SetPositionAndRotation(new Vector3(Camera1.position.x, Camera1.position.y, -10), new Quaternion(0, 0, 0, 0));              //Move the secondary camera to the main cameras position
         }
     }
 }
