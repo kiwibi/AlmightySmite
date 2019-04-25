36a86963a55e52ae75072a79f6b8cb3f4f089da6
Felix Almqvist <kiwibirdi@hotmail.com>
2019-04-25 12:40:53 +0200

    MockUp UI

--- Almighty Smite/Assets/Scripts/WindBehaviour.cs
+++ Almighty Smite/Assets/Scripts/WindBehaviour.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WindBehaviour : MonoBehaviour
 {
                                                                                                                         //privata variabler
     private int WindDirection;
     private Vector2 WindMovement;
-    private SpriteRenderer WindIndicatorRenderer;
+    private Image WindIndicatorRenderer;
 
     [Header("Offset från mitten i väderstjärnan")]                                                                      //public variables
     public float WindIndicatorOffset;                                                                                   //Distans som pekaren är ifrån mitten av kompassen/vindriktningsvisaren
@@ -18,7 +19,7 @@ public class WindBehaviour : MonoBehaviour
     void Start()
     {
         WindDirection = 0;                                                                                              //sätter vinden till at börja peka mot norr
-        WindIndicatorRenderer = GetComponentsInChildren<SpriteRenderer>()[1];                                           //getcomponent av pekarens sprite som ska flyttas och roteras
+        WindIndicatorRenderer = GetComponentsInChildren<Image>()[1];                                           //getcomponent av pekarens sprite som ska flyttas och roteras
         instance = this;                                                                                                //klargör att instance är just det här scriptet
     }
 
@@ -52,42 +53,42 @@ public class WindBehaviour : MonoBehaviour
         {
             case 0:                                                //nord
                 WindMovement.Set(0, 1);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(0, WindIndicatorOffset);                                                //localPosition = positionen som objektet ska ha beroende på vilken riktning. i det här fallet är det pekaren
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(0, WindIndicatorOffset);                                                //localPosition = positionen som objektet ska ha beroende på vilken riktning. i det här fallet är det pekaren
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 0);                                                          //localRotation = rotationen som objektet ska ha beroende på vilken riktning. i det här fallet är det pekaren 
                 break;
             case 1:                                                //nordöst
                 WindMovement.Set(1, 1);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(WindIndicatorOffset - 0.5f, WindIndicatorOffset - 0.5f);
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(WindIndicatorOffset - 0.5f, WindIndicatorOffset - 0.5f);
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 315);
                 break;
             case 2:                                                //öst
                 WindMovement.Set(1, 0);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(WindIndicatorOffset, 0);
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(WindIndicatorOffset, 0);
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 270);
                 break;
             case 3:                                                //sydöst
                 WindMovement.Set(1, -1);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(WindIndicatorOffset - 0.5f, -(WindIndicatorOffset - 0.5f));
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(WindIndicatorOffset - 0.5f, -(WindIndicatorOffset - 0.5f));
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 225);
                 break;
             case 4:                                                //syd
                 WindMovement.Set(0, -1);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(0, -WindIndicatorOffset);
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(0, -WindIndicatorOffset);
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 180);
                 break;
             case 5:                                                //sydväst
                 WindMovement.Set(-1, -1);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(-(WindIndicatorOffset - 0.5f), -(WindIndicatorOffset - 0.5f));
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(-(WindIndicatorOffset - 0.5f), -(WindIndicatorOffset - 0.5f));
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 135);
                 break;
             case 6:                                                //väst
                 WindMovement.Set(-1, 0);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(-WindIndicatorOffset, 0);
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(-WindIndicatorOffset, 0);
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 90);
                 break;
             case 7:                                                //nordväst
                 WindMovement.Set(-1, 1);
-                WindIndicatorRenderer.transform.localPosition = new Vector3(-(WindIndicatorOffset - 0.5f), WindIndicatorOffset - 0.5f);
+                //WindIndicatorRenderer.transform.localPosition = new Vector3(-(WindIndicatorOffset - 0.5f), WindIndicatorOffset - 0.5f);
                 WindIndicatorRenderer.transform.localRotation = Quaternion.Euler(0, 0, 45);
                 break;
         }
