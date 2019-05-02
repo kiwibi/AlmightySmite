fee6a953a35dfb9d43e7011d94e7ba8c40ef4be3
Emil Dymek <emil.dymek@gmail.com>
2019-05-02 16:40:55 +0200

    Lose and WIn

--- Almighty Smite/Assets/Scripts/ProgressbarBehaviour.cs
+++ Almighty Smite/Assets/Scripts/ProgressbarBehaviour.cs
@@ -6,17 +6,32 @@ using UnityEngine.UI;
 public class ProgressbarBehaviour : MonoBehaviour
 {
     public Image ProgressBar;
+    public GameObject WinText;
+    public GameObject LoseText;
     public float ProgressPool = 0.0f;
     public float AddPoolTMP = 0.1f;
 
     void Awake()
     {
+        LoseText.SetActive(false);
+        WinText.SetActive(false);
+        Time.timeScale = 1;
         ProgressBar.fillAmount = 0;
     }
 
     void Update()
     {
         ProgressBar.fillAmount = ProgressPool;
-        Debug.Log(ProgressPool);        
+        Debug.Log(ProgressPool);
+        if (ProgressPool >= 1)
+        {
+            LoseText.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (ProgressPool < 0)
+        {
+            WinText.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
