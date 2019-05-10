7c409a778f40470683faed9e844221ef1a36a9b2
Emil Dymek <emil.dymek@gmail.com>
2019-05-10 16:40:55 +0200

    Redo + Map Master

---
+++ Almighty Smite/Assets/Scripts/MapMaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMaster : MonoBehaviour
+{
+    Transform MiniMap;
+
+    void Awake()
+    {
+        MiniMap = transform.Find("Minimapmap");
+        MiniMap.gameObject.SetActive(true);
+    }
+}
