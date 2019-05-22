edd207660af3f7ed687ce5ff16185945550cdb40
Emil Dymek <emil.dymek@gmail.com>
2019-05-22 17:10:27 +0200

    SKOG

---
+++ Almighty Smite/Assets/Scripts/TreeBehaviour.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBehaviour : MonoBehaviour
+{
+    private TreeMaster TreesAlive;
+    private Transform Alive;
+    private Transform Dead;
+    private float Timer = 5.0f;
+    private BoxCollider2D TreeCollider;
+
+    void Start()
+    {
+        TreesAlive = GameObject.Find("Tree Master").GetComponent<TreeMaster>();
+        TreesAlive.TreesAlive++;
+        Alive = transform.GetChild(0);
+        Dead = transform.GetChild(1);
+        Dead.gameObject.SetActive(false);
+        TreeCollider = GetComponentInChildren<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (Alive.gameObject.activeSelf == false)
+        {
+            DeadUpdate();
+        }
+    }
+
+    private void DeadUpdate()
+    {
+        Timer -= 1 * Time.deltaTime;
+        if (Timer < 0)
+        {
+            Timer = 8.0f;
+            TreesAlive.TreesAlive++;
+            SwitchState();
+        }
+    }
+
+    private void SwitchState()
+    {
+        if (Alive.gameObject.activeSelf == true)
+        {
+            Alive.gameObject.SetActive(false);
+            Dead.gameObject.SetActive(true);
+        }
+        else
+        {
+            Alive.gameObject.SetActive(true);
+            Dead.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Disaster" && Alive.gameObject.activeSelf == true)
+        {
+            SwitchState();
+        }
+    }
+}
---
+++ Almighty Smite/Assets/Scripts/TreeMaster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeMaster : MonoBehaviour
+{
+    public int TreesAlive = 0;
+
+    void Update()
+    {
+        
+    }
+}
