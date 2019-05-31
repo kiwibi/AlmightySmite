5d41214d64f2109a47538e71742e51c22700ec66
Emil Dymek <emil.dymek@gmail.com>
2019-05-31 16:27:45 +0200

    Wooo

--- Almighty Smite/Assets/Scripts/EndTitleVoid.cs
+++ Almighty Smite/Assets/Scripts/EndTitleVoid.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndTitleVoid : MonoBehaviour
 {
+    public Image Void;
+    Color Saturation;
+
+
     void Start()
     {
         
@@ -12,5 +17,16 @@ public class EndTitleVoid : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = 0f; f <= 1.0; f += 0.01f)
+        {
+            Saturation = Void.color;
+            Saturation.a = f;
+            Void.color = Saturation;
+            yield return null;
+        }
     }
 }
--- Almighty Smite/Assets/Scripts/LoseText.cs
+++ Almighty Smite/Assets/Scripts/LoseText.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoseText : MonoBehaviour
 {
+    public Image LoseTxt;
+    Color Saturation;
+
     void Start()
     {
         
@@ -12,5 +16,16 @@ public class LoseText : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = 0f; f <= 1.0; f += 0.01f)
+        {
+            Saturation = LoseTxt.color;
+            Saturation.a = f;
+            LoseTxt.color = Saturation;
+            yield return null;
+        }
     }
 }
